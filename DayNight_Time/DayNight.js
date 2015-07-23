32e0f8a8b5e45832a38906f1a20f348c080c22ee
William <wfsaunders@gmail.com>
2015-07-23 22:33:41 +0200

    time solved

--- DayNight_Time/DayNight.js
+++ DayNight_Time/DayNight.js
@@ -1,15 +1,17 @@
 //alert("here"); //testing
 $(document).ready(function() {
- $('#orb').click(function(){
+
+
+ 	var hour = new Date().getHours();
        //hasClass, removeClass and addClass are JQuery objects
-       if ($(this).hasClass('sun')) {
-            $(this).removeClass('sun').addClass('moon');
+       if (hour > 18 || hour < 6) {
+            $('#orb').removeClass('sun').addClass('moon');
             $('#sky').removeClass('day').addClass('night');
         }
         else {
-            $(this).removeClass('moon').addClass('sun');
+            $('#orb').removeClass('moon').addClass('sun');
             $('#sky').removeClass('night').addClass('day');
         }
- });
+ ;
 });
 
