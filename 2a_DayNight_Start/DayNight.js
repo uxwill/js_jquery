fd06603e17690e0c20390389927108b2f8900f5c
uxwill <wfsaunders@gmail.com>
2015-07-23 16:16:46 +0200

    1st

---
+++ 2a_DayNight_Start/DayNight.js
@@ -0,0 +1,24 @@
+//alert("here"); //testing
+
+
+ $( document ).ready( function() {
+   
+$('button').click(function() {
+
+	if($('body').hasClass('black')){
+
+		$('body').removeClass('black').addClass('white');
+
+	
+
+	} else {
+
+
+		$('body').removeClass('white').addClass('black');
+}
+}
+
+	);
+
+});
+
